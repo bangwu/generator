fc1402deca2d1eb06c904b2587a23027626516a1
huike <ohwubang@gmail.com>
2021-12-11 00:33:34 +0800

    change the field

--- core/mybatis-generator-core/src/main/java/org/mybatis/generator/plugins/SerializablePlugin.java
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/plugins/SerializablePlugin.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2006-2020 the original author or authors.
+ *    Copyright 2006-2021 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -113,7 +113,8 @@ public class SerializablePlugin extends PluginAdapter {
                 context.getCommentGenerator().addFieldComment(field, introspectedTable);
             }
 
-            topLevelClass.addField(field);
+//            topLevelClass.addField(field);
+            topLevelClass.getFields().add(0, field);
         }
     }
 
